f32c52a92c755a911af449b3014046aa89ba54f3
Cata69 <47688161+Cata69@users.noreply.github.com>
2023-09-06 18:18:16 +0300

    Update triunghi1.cpp

--- triunghi1.cpp
+++ triunghi1.cpp
@@ -6,15 +6,16 @@ int main()
 {
     int n;
     cin>>n;
-    int s = 1;
-    for(int i=1; i<=n; i++){
-        for(int j=i; j<=n; j++){cout<<" ";}
-        for(int j=1;j<=2*i-1;j++) cout<<i;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i; j++){cout<<'#';}
+        for(int j=1;j<=2*i-1; j++){cout<<'*';}
+        for(int j=n-i; j>=1; j--){cout<<'#';}
         cout<<endl;
     }
-    for(int i=n-1; i>=1; i--){
-        for(int j=i; j<=n; j++){cout<<" ";}
-        for(int j=1;j<=2*i-1;j++) cout<<i;
+    for(int i=n-1;i>=1;i--){
+        for(int j=1;j<=n-i; j++){cout<<'#';}
+        for(int j=1;j<=2*i-1; j++){cout<<'*';}
+        for(int j=n-i; j>=1; j--){cout<<'#';}
         cout<<endl;
     }
     return 0;
