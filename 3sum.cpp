cdd18f14b8384f144a860e3dc4303d309426d9db
Cata69 <47688161+Cata69@users.noreply.github.com>
2023-09-14 20:34:54 +0300

    Create 3sum.cpp

---
+++ 3sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void afiseaza_triplete(int n) {
+    bool exista_triplete = false;
+
+    for (int x = 1; x < n - 2; x++) {
+        for (int y = x + 1; y < n - 1; y++) {
+            int z = n - x - y;
+            if (z > y && x + y + z == n && n % x == 0 && n % y == 0 && n % z == 0) {
+                cout << x << " " << y << " " << z << endl;
+                exista_triplete = true;
+            }
+        }
+    }
+
+    if (!exista_triplete) {
+        cout << "nu exista" << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Introduceti un numar natural n: ";
+    cin >> n;
+    afiseaza_triplete(n);
+    return 0;
+}
